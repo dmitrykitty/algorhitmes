f29773681f053b5015ea62f262f72aedc052595b
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2025-05-04 23:54:23 +0200

    linked list added

--- linked_list/l-list2.py
+++ linked_list/l-list2.py
@@ -27,60 +27,34 @@ class MyLinkedList(object):
             cur = cur.next
 
     def get(self, index):
-        if  index < 0 or index > self.length - 1:
+        if index < 0 or index >= self.length:
             return - 1
-        counter = 0
+
         cur = self.head
-        while cur and counter < index:
+        for i in range(index):
             cur = cur.next
-            counter += 1
-        if cur:
-            return cur.val
+        return cur.val
 
     def addAtHead(self, val):
-        node = ListNode(val)
-        if not self.head:
-            self.head = node
-        else:
-            node.next = self.head
-            self.head = node
-        self.length += 1
+        self.addAtIndex(0, val)
 
     def addAtTail(self, val):
-        node = ListNode(val)
-        if not self.head:
-            self.head = node
-        else:
-            cur = self.head
-            while cur.next:
-                cur = cur.next
-
-            cur.next = node
-
-        self.length += 1
+        self.addAtIndex(self.length, val)
 
     def addAtIndex(self, index, val):
-        if  index < 0 or index > self.length - 1:
-            return
-        node = ListNode(val)
-        if index == 0:
-            node.next = self.head
-            self.head = node
-            self.length += 1
+        if index < 0 or index > self.length:
             return
 
-        cur = self.head
-        counter = 0
-        while cur and counter < index - 1:
-            cur = cur.next
-            counter += 1
-
-        node.next = cur.next
-        cur.next = node
         self.length += 1
 
+        cur = self.head
+        for i in range(index - 1):
+            cur = cur.next
+        temp = cur.next
+        cur.next = ListNode(val, temp)
+
     def deleteAtIndex(self, index):
-        if  index < 0 or index > self.length - 1:
+        if index < 0 or index > self.length - 1:
             return
         if index == 0:
             self.head = self.head.next
@@ -99,13 +73,9 @@ class MyLinkedList(object):
         self.length -= 1
 
 
-
-
 lst1 = MyLinkedList()
 lst1.addAtHead(2)
 lst1.addAtTail(3)
 lst1.addAtIndex(3, 2)
 lst1.showList(lst1.head)
 print(lst1.get(1))
-
-
